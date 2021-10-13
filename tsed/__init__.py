1f4366533ebe4d334039b60d296b9b3cf117aeb9
Ivan Filho <ivanfilhoreis@gmail.com>
2021-10-13 19:44:46 -0300

    Update __init__.py

--- tsed/__init__.py
+++ tsed/__init__.py
@@ -1,27 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Editor Spyder
+__version__ = "1.0"
 
-Este é um arquivo de script temporário.
-"""
-
-class tsed():
-    
-    def __init__(self, ts, text):
-        self.ts = ts;
-        self.text = text;
-        
-    def __str__(self):
-        return "Module: TSED (Time-Series Enriched with Domain-specific terms)"
-        
-
-
-
-def main():
-    
-    tt = tsed(0, "teste")
-    
-    print(tt)
-    
-    
-main()
+from .tsbow import Tsed_BoW
+from .tskeybert import Tsed_Bert
