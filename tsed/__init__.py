ca8f73cec4d65bd4d84d63098e9403601c7e52e7
Ivan Filho <ivanfilhoreis@gmail.com>
2021-11-13 14:35:16 -0300

    Update __init__.py

--- tsed/__init__.py
+++ tsed/__init__.py
@@ -1,5 +1,5 @@
 from tsed._bow import tsed_bow
-from tsed._keyBert import tsed_keyBert
-from tsed._bert import tsed_bert
+#from tsed._keyBert import tsed_keyBert
+#from tsed._bert import tsed_bert
 
 __version__ = "1.0"
